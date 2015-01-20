b07a8515fba43dd803dedee941e146f06283c312
Chintan Shah <chintanhappiness@gmail.com>
2015-01-20 10:24:26 +0530

    new one with circle on the map

--- js/controllers.js
+++ js/controllers.js
@@ -38,6 +38,7 @@ phonecatControllers.controller('home', ['$scope', 'TemplateService', 'Navigation
             };
 
             $scope.markers = NavigationService.formatmarkers(data);
+            iconchange();
 
         };
         NavigationService.getproperties().success(propertiessuccess);
@@ -47,6 +48,86 @@ phonecatControllers.controller('home', ['$scope', 'TemplateService', 'Navigation
             $scope.form.latlong = data.latitude + "," + data.longitude;
             NavigationService.getdetails($scope.form.latlong, $scope.form.type).success(placescomplete);
         };
+
+
+
+
+
+
+
+        $scope.circles = [
+            {
+                id: 1,
+                center: {
+                    latitude: 19.075984,
+                    longitude: 72.877656
+                },
+                radius: 2500,
+                stroke: {
+                    color: '#08B21F',
+                    weight: 2,
+                    opacity: 1
+                },
+                fill: {
+                    color: '#08B21F',
+                    opacity: 0.5
+                },
+                events: {
+
+                    center_changed: function() {
+                        iconchange();
+                    }
+                },
+                geodesic: true, // optional: defaults to false
+                draggable: true, // optional: defaults to false
+                clickable: true, // optional: defaults to true
+                editable: true, // optional: defaults to false
+                visible: true // optional: defaults to true
+            }
+
+        ];
+
+
+        var iconchange = function () {
+            for (var i = 0; i < $scope.markers.length; i++) {
+                var d = getDistance($scope.markers[i].coords.latitude, $scope.markers[i].coords.longitude, $scope.circles[0].center.latitude, $scope.circles[0].center.longitude);
+                var r = $scope.circles[0].radius / 1000;
+                if (d > r) {
+                    $scope.markers[i].icon = 'img/favicon.ico';
+                    console.log(i + '=' + d);
+                } else {
+                    $scope.markers[i].icon = 'img/logo.png';
+                }
+            }
+        };
+        
+        var getDistance = function (lat1, long1, lat2, long2) {
+            var R = 6378.137; // Earth’s mean radius in km
+            var p1 = {
+                lat: lat1,
+                lng: long1
+            };
+            var p2 = {
+                lat: lat2,
+                lng: long2
+            };
+
+            var dLat = rad(p2.lat - p1.lat);
+            var dLong = rad(p2.lng - p1.lng);
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+                Math.sin(dLong / 2) * Math.sin(dLong / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            var d = R * c;
+            return d; // returns the distance in km
+
+
+
+        };
+        var rad = function (x) {
+            return x * Math.PI / 180;
+        };
+
 
   }]);
 phonecatControllers.controller('headerctrl', ['$scope', 'TemplateService',
